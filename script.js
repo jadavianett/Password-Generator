f8b09dc2082928b53fd2b846365510be4aa6560d
Jada Arnett <jada.arnett@gmail.com>
2020-09-02 22:34:48 -0400

    added all variable to hold passwords and defined character variables

--- script.js
+++ script.js
@@ -7,54 +7,80 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+function generatePassword() {
+  var password = "";
+  var passwordLength = prompt(
+    "How many characters would you like your password to contain? Enter a number between 8 and 128."
+  );
 
-function generatePassword(onclick="generateBtn") {
-  var passwordLength = prompt("How many characters would you like your password to contain? Enter a number between 8 and 128."); 
-  
-  
-// options for pass word length (at least 8 characters and no more than 128 characters)
+  // options for pass word length (at least 8 characters and no more than 128 characters)
 
-// prompt("How many characters would you like your password to contain? Enter a value between 8 and 128.")
+  // prompt("How many characters would you like your password to contain? Enter a value between 8 and 128.")
 
-//options for pass word types to include (lowercase, uppercase, numeric, and/or special characters)
+  //options for pass word types to include (lowercase, uppercase, numeric, and/or special characters)
 
-//  "Click okay to include special characters" (okay and cancel options, use boolean?)
-var specialCharacters = "!@#$%^&*()_?<>";
-var confirmCharacters = confirm("Click OK to confirm including special characters.");
-//  "Click okay to include numbers" (okay and cancel options, use boolean?)
-var numbers = "1234567890"
-var confirmNumbers = confirm("Click OK to confirm including numbers.");
-//  "Click okay to include uppercase letters" (okay and cancel options, use boolean?)
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var confirmUppercaseLetters = confirm("Click OK to confirm including uppercase letters.");
-//  "Click okay to include lowercase letters"(okay and cancel options, use boolean?)
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
-var confirmlowercaseLetters = confirm("Click OK to confirm including lowercase letters.");
+  //  "Click okay to include special characters" (okay and cancel options, use boolean?)
 
+  var finalPassword = "";
 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please select a number between 8 and 128.");
+    generatePassword();
+  } else {
+    var specialCharacters = "!@#$%^&*()_?<>";
+    var confirmCharacters = confirm(
+      "Click OK to confirm including special characters."
+    );
+    //  "Click okay to include numbers" (okay and cancel options, use boolean?)
+    var numbers = "1234567890";
+    var confirmNumbers = confirm("Click OK to confirm including numbers.");
+    //  "Click okay to include uppercase letters" (okay and cancel options, use boolean?)
+    var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var confirmUppercaseLetters = confirm(
+      "Click OK to confirm including uppercase letters."
+    );
+    //  "Click okay to include lowercase letters"(okay and cancel options, use boolean?)
+    var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    var confirmLowercaseLetters = confirm(
+      "Click OK to confirm including lowercase letters."
+    );
+    if (confirmCharacters == true) {
+      password = password + specialCharacters;
+    }
+    if (confirmNumbers == true) {
+      password = password + numbers;
+    }
 
-//pass word generated and displays in generate password box 
-  // TODo: generate a real password and replace return string with a real password string 
-  return "THIS IS NOT MY FINAL PASSWORD!"
+    if (confirmUppercaseLetters == true) {
+      password = password + uppercaseLetters;
+    }
+    if (confirmLowercaseLetters == true) {
+      password = password + lowercaseLetters;
+    }
+
+    if (
+      confirmCharacters == false &&
+      confirmNumbers == false &&
+      confirmUppercaseLetters == false &&
+      confirmLowercaseLetters == false
+    ) {
+      alert("Please select at least one option.");
+      return "";
+    }
+
+    for (var i = 0; i < passwordLength; i++) {
+      var randomCharacter = Math.floor(Math.random() * password.length);
+      finalPassword = finalPassword + password[randomCharacter];
+    }
+
+    //pass word generated and displays in generate password box
+    // TODo: generate a real password and replace return string with a real password string
+    return finalPassword;
+  }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// something like this for random key gen
-
-// function makeid(length) {
-//    var result           = '';
-//    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//    var charactersLength = characters.length;
-//    for ( var i = 0; i < length; i++ ) {
-//       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//    }
-//    return result;
-// }
-
-
 
 
